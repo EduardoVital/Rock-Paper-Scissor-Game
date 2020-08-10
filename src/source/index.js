b5206bae76a94d3dbb98ad7ae6bea3a90b1fd776
Eduardo Vital de Souza <eduardovitals@gmail.com>
2020-08-10 18:52:11 -0300

    play again button working

--- src/source/index.js
+++ src/source/index.js
@@ -1,4 +1,4 @@
-const cardBtn = document.querySelectorAll('.card__border')
+const cardBtn = document.querySelectorAll('.options .card__border')
 const score = document.getElementById('score')
 const button = document.querySelector('.btn')
 const options = document.querySelector('.options')
@@ -7,175 +7,138 @@ const cardChoice = document.querySelector('.choice .card__border')
 const wait = document.querySelector('.computer__wait')
 const cardComputerChoice = document.querySelector('.computer .card__border')
 const WinOrLose = document.querySelector('.check')
+const playAgain = document.querySelector('.check button')
 
 const pcChoice = ['rock', 'paper', 'scissor']
 let userChoice = undefined
 let count = 0
 
 const resultadoPcChoice = () => {
-    return pcChoice[Math.floor(Math.random() * pcChoice.length)]
+  return pcChoice[Math.floor(Math.random() * pcChoice.length)]
 }
 
 const computerChoice = resultadoPcChoice()
 
 const updateScore = (value) => {
-    count += value
-    score.innerHTML = count
+  count += value
+  score.innerHTML = count
 }
 
 const resultWin = () => {
-    const tittleResult = document.querySelector('.check h2')
-    tittleResult.innerHTML = 'You Win'
-    choice.style.width = '65%'
-    WinOrLose.style.display = 'flex'
+  const tittleResult = document.querySelector('.check h2')
+  tittleResult.innerHTML = 'You Win'
+  choice.style.width = '65%'
+  WinOrLose.style.display = 'flex'
 }
 
 const resultDraw = () => {
-    const tittleResult = document.querySelector('.check h2')
-    tittleResult.innerHTML = 'Game Draw'
-    choice.style.width = '65%'
-    WinOrLose.style.display = 'flex'
+  const tittleResult = document.querySelector('.check h2')
+  tittleResult.innerHTML = 'Game Draw'
+  choice.style.width = '65%'
+  WinOrLose.style.display = 'flex'
 }
 
 const resultLose = () => {
-    const tittleResult = document.querySelector('.check h2')
-    tittleResult.innerHTML = 'You Lose'
-    choice.style.width = '65%'
-    WinOrLose.style.display = 'flex'
+  const tittleResult = document.querySelector('.check h2')
+  tittleResult.innerHTML = 'You Lose'
+  choice.style.width = '65%'
+  WinOrLose.style.display = 'flex'
 }
 
 const checkWinner = () => {
-
-    if( userChoice === computerChoice ) {
-        //draw
-        setTimeout(() => {
-            resultDraw()
-        },1500)
-    } else if ( (userChoice === 'paper' && computerChoice === 'rock') || (userChoice === 'rock' && computerChoice === 'scissor') || (userChoice === 'scissor' && computerChoice === 'paper') ) {
-        // user won
-        setTimeout(() => {
-            resultWin()
-            updateScore(1)
-        },1500)
-    } else {
-        // user lose
-        setTimeout(() => {
-            resultLose()
-            updateScore(-1)
-        },1500)
-    }
+  if (userChoice === computerChoice) {
+    //draw
+    setTimeout(() => {
+      resultDraw()
+    }, 1500)
+  } else if (
+    (userChoice === 'paper' && computerChoice === 'rock') ||
+    (userChoice === 'rock' && computerChoice === 'scissor') ||
+    (userChoice === 'scissor' && computerChoice === 'paper')
+  ) {
+    // user won
+    setTimeout(() => {
+      resultWin()
+      updateScore(1)
+    }, 1500)
+  } else {
+    // user lose
+    setTimeout(() => {
+      resultLose()
+      updateScore(-1)
+    }, 1500)
+  }
 }
 
 const modalEvents = (element) => {
-    const modal = document.querySelector(element)
-    modal.classList.add('mostrar')
-    modal.addEventListener('click', (e) => {
-        if(e.target.className === 'modal__close' || e.target.className === 'modal mostrar') {
-            modal.classList.remove('mostrar')
-        }
-    })
+  const modal = document.querySelector(element)
+  modal.classList.add('mostrar')
+  modal.addEventListener('click', (e) => {
+    if (
+      e.target.className === 'modal__close' ||
+      e.target.className === 'modal mostrar'
+    ) {
+      modal.classList.remove('mostrar')
+    }
+  })
 }
 
-const youChoice = () => {
-    if( userChoice === 'paper' ) {
-        options.style.display = 'none'
-        choice.style.display = 'flex'
-        //create elements
-        let div = document.createElement('div')
-        let img = document.createElement('img')
-        img.src = '/src/assets/images/icon-paper.svg'
-        //append elements
-        div.appendChild(img)
-        cardChoice.appendChild(div)
-        //add class
-        cardChoice.classList.add('blue')
-    } else if ( userChoice === 'rock' ) {
-        options.style.display = 'none'
-        choice.style.display = 'flex'
-        //create elements
-        let div = document.createElement('div')
-        let img = document.createElement('img')
-        img.src = '/src/assets/images/icon-rock.svg'
-        //append elements
-        div.appendChild(img)
-        cardChoice.appendChild(div)
-        //add class
-        cardChoice.classList.add('red')
-    } else if ( userChoice === 'scissor' ) {
-        options.style.display = 'none'
-        choice.style.display = 'flex'
-        //create elements
-        let div = document.createElement('div')
-        let img = document.createElement('img')
-        img.src = '/src/assets/images/icon-scissors.svg'
-        //append elements
-        div.appendChild(img)
-        cardChoice.appendChild(div)
-        //add class
-        cardChoice.classList.add('yellow')
-    }
+const youChoice = () => { 
+  options.style.display = 'none'
+  choice.style.display = 'flex'
+
+  cardChoice.classList.remove('paper')
+  cardChoice.classList.remove('rock')
+  cardChoice.classList.remove('scissor')
+
+  cardChoice.classList.add(`${userChoice}`)
+  const img = cardChoice.querySelector('img')
+  img.src = `/src/assets/images/icon-${userChoice}.svg`
+  img.alt = userChoice
 }
 
 const houseChoice = () => {
-    if( computerChoice === 'paper' ) {
-        wait.style.display = 'none'
-        cardComputerChoice.style.display = 'flex'
-        //create elements
-        let div = document.createElement('div')
-        let img = document.createElement('img')
-        img.src = '/src/assets/images/icon-paper.svg'
-        //append elements
-        div.appendChild(img)
-        cardComputerChoice.appendChild(div)
-        //add class
-        cardComputerChoice.classList.add('blue')
-    } else if ( computerChoice === 'rock' ) {
-        wait.style.display = 'none'
-        cardComputerChoice.style.display = 'flex'
-        //create elements
-        let div = document.createElement('div')
-        let img = document.createElement('img')
-        img.src = '/src/assets/images/icon-rock.svg'
-        //append elements
-        div.appendChild(img)
-        cardComputerChoice.appendChild(div)
-        //add class
-        cardComputerChoice.classList.add('red')
-    } else if ( computerChoice === 'scissor' ) {
-        wait.style.display = 'none'
-        cardComputerChoice.style.display = 'flex'
-        //create elements
-        let div = document.createElement('div')
-        let img = document.createElement('img')
-        img.src = '/src/assets/images/icon-scissors.svg'
-        //append elements
-        div.appendChild(img)
-        cardComputerChoice.appendChild(div)
-        //add class
-        cardComputerChoice.classList.add('yellow')
-    }
+  wait.style.display = 'none'
+  cardComputerChoice.style.display = 'flex'
+
+  cardComputerChoice.classList.remove('paper')
+  cardComputerChoice.classList.remove('rock')
+  cardComputerChoice.classList.remove('scissor')
+
+  cardComputerChoice.classList.add(`${computerChoice}`)
+  const img = cardComputerChoice.querySelector('img')
+  img.src = `/src/assets/images/icon-${computerChoice}.svg`
+  img.alt = computerChoice
 }
 
 const click = () => {
-    cardBtn.forEach((item) => {
-        item.addEventListener('click', () => {
-            userChoice = item.getAttribute('data-choice')
-            youChoice()
-            setTimeout(() => {
-                houseChoice()
-                checkWinner()
-            }, 1000)
-        })
+  cardBtn.forEach((item) => {
+    item.addEventListener('click', () => {
+      userChoice = item.getAttribute('data-choice')
+      youChoice()
+      setTimeout(() => {
+        houseChoice()
+        checkWinner()
+      }, 1000)
+    })
+  })
+}
+
+const play = () => {
+    playAgain.addEventListener('click', () => {
+        choice.style.display = 'none'
+        WinOrLose.style.display = 'none'
+        choice.style.width = '50%'
+        wait.style.display = 'flex'
+        cardComputerChoice.style.display = 'none'
+        options.style.display = 'flex'
     })
 }
 
 const showModal = () => {
-    button.addEventListener('click', () => modalEvents('.modal'))
+  button.addEventListener('click', () => modalEvents('.modal'))
 }
 
 click()
+play()
 showModal()
-
-
-
-
